4f3101b8e8c59bc8467f868b43edcff5453f7bae
Patryk Eliasz <patryk0493@gmail.com>
2019-12-13 15:05:35 +0100

    some fixes

--- src/components/sidebar/TreeNode.js
+++ src/components/sidebar/TreeNode.js
@@ -1,21 +1,20 @@
 import React from "react";
-import {  Link } from "gatsby";
+import { Link } from "gatsby";
 import classNames from 'classnames';
 
 import {
   NavItem,
 } from "reactstrap";
 
-const TreeNode = ({className = '', setCollapsed, collapsed, url, lvl, title, items, location, docsRoute, ...rest}) => {
+const TreeNode = ({ className = '', setCollapsed, collapsed, url, lvl, title, items, location, docsRoute, ...rest }) => {
   const isCollapsed = Object.keys(collapsed).includes(url) && collapsed[url];
-  
 
-  // const collapse = () => {
-  //   setCollapsed(url);
-  // }
-  
-  const hasChildren = items.length !== 0;
-  
+  const collapse = () => {
+    setCollapsed(url);
+  }
+
+  const hasChildren = items.length > 0;
+
   const active = location && location.pathname === (docsRoute + url);
   const calculatedClassName = classNames({
     'nav-docs-item': true,
@@ -31,23 +30,26 @@ const TreeNode = ({className = '', setCollapsed, collapsed, url, lvl, title, ite
     <NavItem className={calculatedClassName}>
       {title && (
         <>
-          {/* <span onClick={collapse} className='collapser pr-1 float-left'> 
-            {!isCollapsed ? <i className="fas fa-chevron-right"></i> : <i className="fas fa-chevron-down"></i>}
-          </span> */}
+          {
+            // <span onClick={collapse} className='collapser pr-1 float-left'>
+            //   {!isCollapsed ? <i className="fas fa-chevron-right"></i> : <i className="fas fa-chevron-down"></i>}
+            // </span>
+          }
           <Link to={docsRoute + url} className="text-white d-inline-block">
             <h4 className="mb-0">
-              <span className={calculatedTitleClassName} style={{fontSize: '0.75em'}}>{title}</span>
+              <span className={calculatedTitleClassName} style={{ fontSize: '0.75em' }}>{title}</span>
             </h4>
           </Link>
         </>
       )}
-  
-      {!isCollapsed && hasChildren ? (
-        <ul className={`${lvl > 1 ? 'pl-2' : '' } mb-0 list-unstyled`}>
+
+      {!isCollapsed ? (
+        <ul className={`${lvl > 1 ? 'pl-2' : ''} mb-0 list-unstyled`}>
           {items.map((item, idx) => {
-              return ( <TreeNode  key={idx} setCollapsed={setCollapsed} lvl={lvl+1} collapsed={collapsed} 
-                                  docsRoute={docsRoute} location={location} url={url} {...item} />
-          )})}
+            return (<TreeNode key={idx} setCollapsed={setCollapsed} lvl={lvl + 1} collapsed={collapsed}
+              docsRoute={docsRoute} location={location} url={url} {...item} />
+            )
+          })}
         </ul>
       ) : null}
     </NavItem>
