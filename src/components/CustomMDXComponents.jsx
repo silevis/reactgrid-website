f7081c0faa878e51e840db4c79405cae5592dbd8
patryk0493@gmail.com <patryk0493gmail.com>
2020-05-20 10:23:10 +0200

    styling update

--- src/components/CustomMDXComponents.jsx
+++ src/components/CustomMDXComponents.jsx
@@ -7,54 +7,54 @@ export default function CustomMDXComponents({ children }) {
       components={{
         h1: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h1 {...props} className="pt-4 mb-2" style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.5em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h1 {...props} className="pt-4 mb-2" style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.5em' }}>
               {props.children}
             </h1>
           </>
         ),
         h2: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h2 {...props} className="pt-4 mb-2" style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h2 {...props} className="pt-4 mb-2" style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em' }}>
               {props.children}
             </h2>
           </>
         ),
         h3: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h3 {...props} className="pt-4 mb-2" style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h3 {...props} className="pt-4 mb-2" style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em' }}>
               {props.children}
             </h3>
           </>
         ),
         h4: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h4 {...props} style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h4 {...props} style={{ paddingBottom: '0.3em' }}>
               {props.children}
             </h4>
           </>
         ),
         h5: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h5 {...props} style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h5 {...props} style={{ paddingBottom: '0.3em' }}>
               {props.children}
             </h5>
           </>
         ),
         h6: props => (
           <>
-            <span className="anchor" id={props.children && props.children.toString().replace(/ /g,"").toLowerCase()}></span>
-            <h6 {...props} style={{borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em'}}>
+            <span className="anchor" id={props.children && props.children.toString().replace(/ /g, "").toLowerCase()}></span>
+            <h6 {...props} style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.1)', paddingBottom: '0.3em' }}>
               {props.children}
             </h6>
           </>
         ),
-        p:  props => <p {...props} style={{paddingBottom: '1em', lineHeight: '2em'}}>{props.children}</p>,
-        table: props => <div style={{overflowX: 'auto'}}><Table {...props}>{props.children}</Table></div>,
+        p: props => <p {...props} style={{ paddingBottom: '1em', lineHeight: '2em' }}>{props.children}</p>,
+        table: props => <div style={{ overflowX: 'auto' }}><Table {...props}>{props.children}</Table></div>,
         th: props => <th {...props} className="font-weight-bold">{props.children}</th>,
         li: props => <li {...props}><p>{props.children}</p></li>,
       }}
