d30086c832bdfdfc2a2e59d8cb11372ee80059ac
Patryk Eliasz <patryk0493@gmail.com>
2020-08-05 10:21:25 +0200

    reformated docs side menu

--- src/templates/docs-post.js
+++ src/templates/docs-post.js
@@ -11,12 +11,14 @@ import {
   DropdownMenu,
   DropdownItem,
   Button,
+  UncontrolledCollapse,
 } from "reactstrap";
 import Tree from "../components/sidebar/Tree";
 import SidebarLayout from "../components/DocsSideNav";
 import CustomMDXComponents from "../components/CustomMDXComponents";
 import { isBrowserIE } from "../components/isBrowserIE";
 import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer";
+
 
 class DocsPostTemplate extends React.Component {
   state = {
@@ -33,14 +35,6 @@ class DocsPostTemplate extends React.Component {
   componentWillUnmount() {
     document.body.classList.remove("blog-post");
   }
-
-  /* handleScroll = () => {
-    if (document.documentElement.scrollTop > 145 || document.body.scrollTop > 145) {
-      this.setState({ isDocsNavFloating: true });
-    } else if (document.documentElement.scrollTop < 146 || document.body.scrollTop < 146) {
-      this.setState({ isDocsNavFloating: false });
-    }
-  }; */
 
   render() {
     const { data, location } = this.props;
@@ -61,6 +55,7 @@ class DocsPostTemplate extends React.Component {
     const currentPostIndex = posts.findIndex(item => `${docsPage.route}${item.node.fields.slug}` === location.pathname)
     const previousPost = posts[currentPostIndex - 1];
     const nextPost = posts[currentPostIndex + 1];
+    const tree = <Tree version={version} edges={posts} docsRoute={docsPage.route} currentLocation={location} navOrder={docsPagesOrder} />;
     return (
       <Layout location={location} pages={pages} social={social} description={description} title={title}>
         <SEO title={post.frontmatter.title} description={post.frontmatter.metaDescription} />
@@ -68,16 +63,36 @@ class DocsPostTemplate extends React.Component {
           <Container>
             <div className="space-70 d-none d-md-block"></div>
             <Row>
-              <Col md="3" lg="3" xl="3" className="pb-5 pb-md-0">
-                <UncontrolledDropdown className="dropdown-version-wrapper mt-4 mt-md-0">
-                  <DropdownToggle caret size="md" className="btn-primary text-nowrap px-3 ">
-                    v{version}
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    {dropdownItemsList}
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-                <Tree version={version} edges={posts} docsRoute={docsPage.route} currentLocation={location} navOrder={docsPagesOrder} />
+              <Col md="3" lg="3" xl="3" className="pb-3 pb-md-0">
+                <UncontrolledCollapse navbar toggler="#docsnav" id="docs-collapser"
+                  className="h-100 d-md-none position-relative overflow-hidden position-absolute">
+                  <Button className="navbar-toggler btn-link btn-danger btn-sm text-danger position-absolute mr-3 mt-3" id="docsnav"
+                    style={{ top: 0, right: 0 }}>
+                    <i className="fas fa-times fa-2x p-2"></i>
+                  </Button>
+                  {tree}
+                </UncontrolledCollapse>
+                <Row>
+                  <Col>
+                    <UncontrolledDropdown className="dropdown-version-wrapper mt-4 mt-md-0">
+                      <DropdownToggle caret size="md" className="btn-primary text-nowrap px-3 ">
+                        v{version}
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        {dropdownItemsList}
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </Col>
+                  <Col className='d-md-none'>
+                    <div className='bg-transparent h-100 d-flex justify-content-end align-items-center '>
+                      <span id="docsnav" className='p-3'> <i className="fas fa-bars text-primary fa-3x"></i></span>
+                    </div>
+                  </Col>
+                </Row>
+
+                <div className='d-none d-md-block'>
+                  {tree}
+                </div>
               </Col>
               <Col md="9" lg="7" xl="7" className="pl-md-5 pr-lg-5 pl-lg-2">
                 <h1 id="docs-header"><span className="text-primary">
