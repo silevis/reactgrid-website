07b6c4eb1f67dc8976e1acdc77d8104c997a0e57
Patryk Eliasz <patryk0493@gmail.com>
2021-02-11 08:54:59 +0100

    fixed page titles in navbar

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -21,7 +21,7 @@ class Features extends React.Component<any, PageProps> {
     const { title, description, pages, social } = data.site.siteMetadata;
     const features = data.allFeaturesYaml.edges;
     return (
-      <Layout pages={pages} social={social} description={description} title={'Features'}>
+      <Layout pages={pages} social={social} description={description} title={title}>
         <SEO title={'Features'} />
         <div className="section mt-5 mt-md-0">
           <div className="space-50"></div>
